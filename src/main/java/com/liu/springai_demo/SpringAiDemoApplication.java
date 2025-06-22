ef2f034329c28e6b951c3cfc7127034c770104fb
nI7_4DB <liu1243885596@gmail.com>
2025-06-23 07:53:53 +0800

    init

---
+++ src/main/java/com/liu/springai_demo/SpringAiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.liu.springai_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liu/springai_demo/SpringAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.springai_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
